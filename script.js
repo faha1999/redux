1cda0a789cf091599df32b5fb6860549735613da
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2023-02-19 00:05:06 +0600

    test with 2 counter

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const counterEl = document.getElementById('counter');
 const incrementEl = document.getElementById('increment');
 const decrementEl = document.getElementById('decrement');
 
+const counterEl2 = document.getElementById('counter2');
+const incrementEl2 = document.getElementById('increment2');
+const decrementEl2 = document.getElementById('decrement2');
+
 // initial state
 let count = 0;
+let count2 = 0;
 
 // event listener
 
@@ -16,4 +21,15 @@ incrementEl.addEventListener('click', () => {
 decrementEl.addEventListener('click', () => {
   count--;
   counterEl.innerHTML = count;
+});
+
+// count2
+incrementEl2.addEventListener('click', () => {
+  count2++;
+  counterEl2.innerHTML = count2;
+});
+
+decrementEl2.addEventListener('click', () => {
+  count2--;
+  counterEl2.innerHTML = count2;
 });
