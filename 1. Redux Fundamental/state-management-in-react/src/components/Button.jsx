37708cc7592a8d14585c2a306480236c3f910b30
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2023-02-19 01:21:48 +0600

    applied complex state management

---
+++ 1. Redux Fundamental/state-management-in-react/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Button = ({ children, handler }) => {
+  return (
+    <button
+      className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+      onClick={handler}
+    >
+      {children}
+    </button>
+  );
+};
