87c3ea98c86afdfd6a65f0922c189100a519cd10
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2023-02-20 13:37:46 +0600

    Why Reducers are called reducers

---
+++ 1. Redux Fundamental/6. Rewrite the Counter Application with vanila redux/reducer.js
@@ -0,0 +1,30 @@
+const action = [
+  { type: 'increment', payload: 1 },
+  { type: 'increment', payload: 1 },
+  { type: 'increment', payload: 1 },
+  { type: 'decrement', payload: 1 },
+];
+
+const initialState = {
+  value: 0,
+};
+
+const counterReducer = (state, action) => {
+  if (action.type === 'increment') {
+    return {
+      ...state,
+      value: state.value + action.payload,
+    };
+  } else if (action.type === 'decrement') {
+    return {
+      ...state,
+      value: state.value - action.payload,
+    };
+  } else {
+    return state;
+  }
+};
+
+const finalResult = action.reduce(counterReducer, initialState);
+
+console.log(finalResult);
