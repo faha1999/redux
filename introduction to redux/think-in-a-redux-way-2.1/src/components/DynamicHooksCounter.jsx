dbd6d0ae4691ec5ec9c744ad9d802f92ea571e58
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2023-02-22 01:15:35 +0600

    applied combineReducers

--- introduction to redux/think-in-a-redux-way-2.1/src/components/DynamicHooksCounter.jsx
+++ introduction to redux/think-in-a-redux-way-2.1/src/components/DynamicHooksCounter.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { increment, decrement } from '../redux/counter/actions';
+import { increment, decrement } from '../redux/dynamicCounter/actions';
 
-export default function HooksCounter() {
-  const count = useSelector((state) => state.counter.value);
+export default function DynamicHooksCounter() {
+  const count = useSelector((state) => state.dynamicCounter.value);
   const dispatch = useDispatch();
 
-  const incrementHandler = () => {
-    dispatch(increment());
+  const incrementHandler = (value) => {
+    dispatch(increment(value));
   };
 
-  const decrementHandler = () => {
-    dispatch(decrement());
+  const decrementHandler = (value) => {
+    dispatch(decrement(value));
   };
 
   return (
@@ -20,13 +20,13 @@ export default function HooksCounter() {
       <div className="flex space-x-3">
         <button
           className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-          onClick={incrementHandler}
+          onClick={() => incrementHandler(5)}
         >
           Increment
         </button>
         <button
           className="bg-red-400 text-white px-3 py-2 rounded shadow"
-          onClick={decrementHandler}
+          onClick={() => decrementHandler(2)}
         >
           Decrement
         </button>
