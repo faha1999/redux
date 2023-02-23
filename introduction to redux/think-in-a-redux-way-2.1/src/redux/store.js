9ccf9cae2fc3858328c28a6fad72c701f753462c
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2023-02-24 00:44:44 +0600

    tested devtools

--- introduction to redux/think-in-a-redux-way-2.1/src/redux/store.js
+++ introduction to redux/think-in-a-redux-way-2.1/src/redux/store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import myLogger from './middlewares/myLogger';
 import rootReducer from './rootReducer';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 
-const store = createStore(rootReducer, applyMiddleware(logger, myLogger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, myLogger)),
+);
 
 export default store;
