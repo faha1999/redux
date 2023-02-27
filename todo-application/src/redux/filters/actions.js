5fc2ee79d5ac9042db7ea41b1e1ed93ce0c8d1b0
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2023-02-27 16:03:37 +0600

    complete store setup

--- todo-application/src/redux/filters/actions.js
+++ todo-application/src/redux/filters/actions.js
@@ -1,18 +1,18 @@
-import { COLCORCHANGED, STATUSCHANGED } from './actionTypes';
+import { COLORCHANGED, STATUSCHANGED } from "./actionTypes";
 
 export const colorChanged = (color, changeType) => {
-  return {
-    type: COLCORCHANGED,
-    payload: {
-      color,
-      changeType,
-    },
-  };
+    return {
+        type: COLORCHANGED,
+        payload: {
+            color,
+            changeType,
+        },
+    };
 };
 
 export const statusChanged = (status) => {
-  return {
-    type: STATUSCHANGED,
-    payload: status,
-  };
+    return {
+        type: STATUSCHANGED,
+        payload: status,
+    };
 };
