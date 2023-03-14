e90061037b4758d3b8a38e6d5d95e21cfe33002d
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2023-03-14 23:56:49 +0600

    applied extra reducer

--- intro-to-redux-toolkit/redux/index.js
+++ intro-to-redux-toolkit/redux/index.js
@@ -1,14 +1,11 @@
-const store = require("./store");
-const { increment, decrement } = require("./counter/actions");
+const store = require('./store');
+const { increment } = require('./counter/actions');
 
 // subscribe to state changes
 store.subscribe(() => {
-    console.log(store.getState());
+  console.log(store.getState());
 });
 
 // disptach actions
 store.dispatch(increment());
-
 store.dispatch(increment());
-
-store.dispatch(decrement());
--- intro-to-redux-toolkit/rtk/index.js
+++ intro-to-redux-toolkit/rtk/index.js
@@ -5,22 +5,12 @@ const {
 } = require('./features/dynamicCounter/dynamicCounterSlice');
 
 // initial state
-//console.log(`Initial State: ${JSON.stringify(store.getState())}`);
+// console.log(`Initial State: ${JSON.stringify(store.getState())}`);
 
 // subscribe to state changes
 store.subscribe(() => {
-  console.log(store.getState());
+  // console.log(store.getState());
 });
 
-// dispatch actions
+// disptach actions
 store.dispatch(counterActions.increment());
-
-store.dispatch(counterActions.increment());
-
-store.dispatch(counterActions.decrement());
-
-store.dispatch(dynamicCounterActions.increment(3));
-
-store.dispatch(dynamicCounterActions.increment(5));
-
-store.dispatch(dynamicCounterActions.decrement(2));
