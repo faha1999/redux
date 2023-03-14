063d5e24157b635f00c02c36432578abc7542741
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2023-03-14 23:00:41 +0600

    added dynamic Counter

--- intro-to-redux-toolkit/rtk/app/store.js
+++ intro-to-redux-toolkit/rtk/app/store.js
@@ -2,10 +2,13 @@ const configureStore = require('@reduxjs/toolkit').configureStore;
 
 const counterReducer = require('../features/counter/counterSlice');
 
+const dynamicCounterReducer = require('../features/dynamicCounter/dynamicCounterSlice');
+
 //configure store
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    dynamicCounter: dynamicCounterReducer,
   },
 });
 
